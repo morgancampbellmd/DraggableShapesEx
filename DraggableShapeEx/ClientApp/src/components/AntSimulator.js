1d2074e1df7e339830fd89ed853544916c36126a
morgan <morgancampbellmd@gmail.com>
2021-06-21 12:56:52 -0400

    ant simulator and draggable shapes both implemented

--- DraggableShapeEx/ClientApp/src/components/AntSimulator.js
+++ DraggableShapeEx/ClientApp/src/components/AntSimulator.js
@@ -5,26 +5,144 @@ export class AntSimulator extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
+    this.state = { kMoves: 0, antPositionY: 1, antPositionX: 1, antDirection: 90, board: [4][4]};
+    this.animateKMoves = this.animateKMoves.bind(this);
+    AntSimulator.updateBoardWhite = AntSimulator.updateBoardWhite.bind(this);
+    AntSimulator.updateBoardBlack = AntSimulator.updateBoardBlack.bind(this);
   }
 
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
+  animateClockwise() {
+  }
+  
+  animateCounterclockwise() {
+    
+  }
+  
+  static updateBoardWhite() {
+    document.getElementById("grid-" + this.state.antPositionY.toString() + this.state.antPositionX.toString()).classList.add("flipper");
+    document.getElementById("grid-" + this.state.antPositionY.toString() + this.state.antPositionX.toString()).classList.add("back");
+    document.getElementById("grid-" + this.state.antPositionY.toString() + this.state.antPositionX.toString()).classList.remove("front");
+  }
+  static updateBoardBlack() {
+    document.getElementById("grid-" + this.state.antPositionY.toString() + this.state.antPositionX.toString()).classList.add("flipper");
+    document.getElementById("grid-" + this.state.antPositionY.toString() + this.state.antPositionX.toString()).classList.add("front");
+    document.getElementById("grid-" + this.state.antPositionY.toString() + this.state.antPositionX.toString()).classList.remove("back");
+  }
+  
+  animateKMoves(e) {
+    e.preventDefault();
+    let moves = document.getElementById("moves").value;
+    for (var i = 0; i < moves; i++) {
+      console.log(this.state.board)
+      console.log(this.state.antPositionX + ", " + this.state.antPositionY);
+      if (this.state.antDirection === 90) {
+        if (this.state.board[this.state.antPositionY][this.state.antPositionX] === 1) {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 0;
+          AntSimulator.updateBoardWhite();
+          this.state.antDirection = 180;
+          if(this.state.antPositionY < 3) this.state.antPositionY += 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.top = (-150 + this.state.antPositionY * 100).toString() + "px";
+        } 
+        else {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 1;
+          AntSimulator.updateBoardBlack();
+          this.state.antDirection = 0;
+          if(this.state.antPositionY < 0) this.state.antPositionY -= 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.top = (-150 + this.state.antPositionY * 100).toString() + "px";
+        }
+      } 
+      else if (this.state.antDirection === 180) {
+        if (this.state.board[this.state.antPositionY][this.state.antPositionX] === 1) {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 0;
+          AntSimulator.updateBoardWhite();
+          this.state.antDirection = 270;
+          if(this.state.antPositionX > 0) this.state.antPositionX -= 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.left = (250 + this.state.antPositionX * 100).toString() + "px";
+        } 
+        else {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 1;
+          AntSimulator.updateBoardBlack();
+          this.state.antDirection = 90;
+          if(this.state.antPositionX < 3) this.state.antPositionX += 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.left = (250 + this.state.antPositionX * 100).toString() + "px";
+        }
+
+      } 
+      else if (this.state.antDirection === 270) {
+        if (this.state.board[this.state.antPositionY][this.state.antPositionX] === 1) {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 0;
+          AntSimulator.updateBoardWhite();
+          this.state.antDirection = 0;
+          if(this.state.antPositionY > 0) this.state.antPositionY -= 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.top = (-150 + this.state.antPositionY * 100).toString() + "px";
+        } 
+        else {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 1;
+          AntSimulator.updateBoardBlack();
+          this.state.antDirection = 180;
+          if(this.state.antPositionY < 3) this.state.antPositionY += 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.top = (-150 + this.state.antPositionY * 100).toString() + "px";
+        }
+      } 
+      else if (this.state.antDirection === 0) {
+        if (this.state.board[this.state.antPositionY][this.state.antPositionX] === 1) {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 0;
+          AntSimulator.updateBoardWhite();
+          this.state.antDirection = 90;
+          if(this.state.antPositionX > 0) this.state.antPositionX -= 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.left = (250 + this.state.antPositionX * 100).toString() + "px";
+        } 
+        else {
+          this.state.board[this.state.antPositionY][this.state.antPositionX] = 1;
+          AntSimulator.updateBoardBlack();
+          this.state.antDirection = 270;
+          if(this.state.antPositionX < 3) this.state.antPositionX += 1;
+          document.getElementById("ant").style.transform = "rotate(" + this.state.antDirection.toString() + "deg)";
+          document.getElementById("ant").style.left = (250 + this.state.antPositionX * 100).toString() + "px";
+        }
+      }
+      
+      setTimeout(() => document.getElementById("grid-" + this.state.antPositionX.toString() + this.state.antPositionY.toString()).classList.remove("flipper"), 300);
+    }
   }
 
+  
   render() {
+    this.state.board = Array(4).fill(1).map(row => new Array(4).fill(1));
+    console.log(this.state.board);
     return (
       <div>
-        <h1>Counter</h1>
+        <h1>Simulate K Moves</h1>
 
-        <p>This is a simple example of a React component.</p>
-
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+        <p>Enter number of moves to simulate, then click 'Apply'.</p>
+          <input id="moves" type="number" defaultValue="Enter moves"></input>
+          <input id="apply" type="submit" value="Apply" onClick={this.animateKMoves}></input>
+          <div className="ant-grid-container">
+            <div className="ant-grid-item front" id="grid-00"></div>
+            <div className="ant-grid-item front" id="grid-01"></div>
+            <div className="ant-grid-item front" id="grid-02"></div>
+            <div className="ant-grid-item front" id="grid-03"></div>
+            <div className="ant-grid-item front" id="grid-10"></div>
+            <div className="ant-grid-item front" id="grid-11"></div>
+            <div className="ant-grid-item front" id="grid-12"></div>
+            <div className="ant-grid-item front" id="grid-13"></div>
+            <div className="ant-grid-item front" id="grid-20"></div>
+            <div className="ant-grid-item front" id="grid-21"></div>
+            <div className="ant-grid-item front" id="grid-22"></div>
+            <div className="ant-grid-item front" id="grid-23"></div>
+            <div className="ant-grid-item front" id="grid-30"></div>
+            <div className="ant-grid-item front" id="grid-31"></div>
+            <div className="ant-grid-item front" id="grid-32"></div>
+            <div className="ant-grid-item front" id="grid-33"></div>
+          </div>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_34175.png&f=1&nofb=1" id="ant" alt="this should be an ant"></img>
       </div>
     );
   }
